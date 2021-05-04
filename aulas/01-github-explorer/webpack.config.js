703d245e2797270ec4f7fa8be120ea3270849201
Lucas Basso <lucas.rbasso@outlook.com>
2021-05-04 13:07:39 -0300

    config: typescript, editor, webpack

--- aulas/01-github-explorer/webpack.config.js
+++ aulas/01-github-explorer/webpack.config.js
@@ -7,13 +7,13 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
     mode: isDevelopment ? 'development': 'production',
     devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
@@ -22,7 +22,7 @@ module.exports = {
     plugins:[
 
         isDevelopment && new ReactRefreshWebpackPlugin(),
-        
+
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
@@ -30,9 +30,9 @@ module.exports = {
     module: {
         rules: [
             {
-               test: /\.jsx$/,
+               test: /\.(j|t)sx$/,
                exclude: /node_modules/,
-               use: { 
+               use: {
                    loader: 'babel-loader',
                    options: {
                        plugins: [
