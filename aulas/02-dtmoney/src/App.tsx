69d46b9d5517fea014c97b7457b5afb93d4f08ae
Lucas Basso <lucas.rbasso@outlook.com>
2021-05-10 21:50:06 -0300

    first config

---
+++ aulas/02-dtmoney/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Dashboard } from './components/Dashboard';
+
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
+  );
+};
