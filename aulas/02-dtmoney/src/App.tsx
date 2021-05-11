4f32792fc95cc19fd46dda1580930203cdfac1de
Lucas Basso <lucas.rbasso@outlook.com>
2021-05-11 17:11:02 -0300

    using new components

--- aulas/02-dtmoney/src/App.tsx
+++ aulas/02-dtmoney/src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard';
 
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export const App: React.FC = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true);
+  }, []);
+
+  const handleCloseTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
