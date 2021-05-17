cabce3f1afa87ee6826f12054dd5cd1607d96e72
Lucas Basso <lucas.rbasso@outlook.com>
2021-05-17 16:24:41 -0300

    initial commit

---
+++ aulas/03-ignews/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello world!</h1>
+    </>
+  );
+};
+
+export default Home;
