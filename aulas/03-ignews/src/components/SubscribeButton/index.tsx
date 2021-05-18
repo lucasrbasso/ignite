2c60d83fcaaf3d1e772ee422b772f94ae23bbe76
Lucas Basso <lucas.rbasso@outlook.com>
2021-05-18 18:48:03 -0300

    stripe: redirectToCheckOut

--- aulas/03-ignews/src/components/SubscribeButton/index.tsx
+++ aulas/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/client';
+import { useCallback } from 'react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -7,8 +11,32 @@ interface SubscribeButtonProps {
 export const SubscribeButton: React.FC<SubscribeButtonProps> = ({
   priceId,
 }) => {
+  const [session] = useSession();
+
+  const handleSubscribe = useCallback(async () => {
+    if (!session) {
+      signIn('github');
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      // eslint-disable-next-line no-alert
+      alert(err.message);
+    }
+  }, [session]);
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
