b4c42de8d0e76fe8c89c4fa7b7f611152acafa22
Lucas Basso <lucas.rbasso@outlook.com>
2021-06-09 22:36:31 -0300

    using ChakraUI

---
+++ aulas/04-dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,35 @@
+import {
+  Flex,
+  Input as ChakraInput,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bg="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: 'gray.900',
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+};
---
+++ aulas/04-dashgo/src/pages/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Header } from '../components/Header';
+
+const Dashboard: React.FC = () => {
+  return <Header />;
+};
+
+export default Dashboard;
