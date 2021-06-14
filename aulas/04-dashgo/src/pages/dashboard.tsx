4ec262b24787778a54f2a8aa5c5480935887713a
Lucas Basso <lucas.rbasso@outlook.com>
2021-06-14 18:10:40 -0300

    pages:  responsiveness added

--- aulas/04-dashgo/src/pages/dashboard.tsx
+++ aulas/04-dashgo/src/pages/dashboard.tsx
@@ -69,13 +69,13 @@ const Dashboard: React.FC = () => {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
             <Chart type="area" series={series} options={options} height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
--- aulas/04-dashgo/src/pages/users/create.tsx
+++ aulas/04-dashgo/src/pages/users/create.tsx
@@ -8,6 +8,7 @@ import {
   HStack,
   Button,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
 import { Input } from '../../components/Form/Input';
 
@@ -22,7 +23,7 @@ export default function UserCreate() {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
-        <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bgColor="gray.800" p={['6', '8']}>
           <Heading size="lg" fontWeight="normal">
             Criar Usuário
           </Heading>
@@ -30,11 +31,11 @@ export default function UserCreate() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" label="E-mail" type="email" />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_confirmation"
@@ -46,7 +47,12 @@ export default function UserCreate() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users">
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
+
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
