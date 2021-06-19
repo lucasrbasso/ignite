c84904a2882eb4aa57ccd48788a587ba7afd2a13
Lucas Basso <lucas.rbasso@outlook.com>
2021-06-19 18:45:21 -0300

    components cities done

---
+++ desafios/desafio-06-worldtrip/src/components/Cities/City.tsx
@@ -0,0 +1,38 @@
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+
+interface CityProps {
+  city: string;
+  country: string;
+}
+
+export const City: React.FC<CityProps> = ({city, country}) => {
+  return (
+    <Box
+      borderRadius="4px"
+      overflow="hidden"
+    >
+      <Image
+        src="/paris.png"
+        alt="paris"
+        bgImage="/paris.png"
+        h="173px"
+        w="100%"
+      />
+      <Flex
+        p="6"
+        align="center"
+        justify="space-between"
+        bg="white"
+        border="1px"
+        borderColor="yellow.300"
+        borderTop="0"
+      >
+      <Flex direction="column">
+        <Heading fontSize="xl" fontWeight="500">{city}</Heading>
+        <Text mt="3" fontSize="md" color="gray.500" fontWeight="500">{country}</Text>
+      </Flex>
+      <Image src="/flagParis.png" alt="paris" w="30px" h="30px" borderRadius="50%" objectFit="cover"/>
+    </Flex>
+    </Box>
+  )
+}
--- desafios/desafio-06-worldtrip/src/components/Cities/index.tsx
+++ desafios/desafio-06-worldtrip/src/components/Cities/index.tsx
@@ -1,16 +1,21 @@
 import { Grid, Heading, Box } from "@chakra-ui/react";
+import { City } from "./City";
 
 export default function Cities() {
   return (
     <>
       <Heading fontWeight="500" fontSize={["2xl","4xl"]} mb="10" mt={["2rem","5rem"]}>Cidades +100</Heading>
-      <Grid templateColumns={["1fr","1fr 1fr", "repeat(3, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]} gap={['20px','45px']} alignItems="center" justifyContent="center" px={["30px", "0"]}>
-        <Box borderRadius="4px" overflow="hidden" bgColor="orange.400" h="170px" w="100%"/>
-        <Box borderRadius="4px" overflow="hidden" bgColor="orange.400" h="170px" w="100%"/>
-        <Box borderRadius="4px" overflow="hidden" bgColor="orange.400" h="170px" w="100%"/>
-        <Box borderRadius="4px" overflow="hidden" bgColor="orange.400" h="170px" w="100%"/>
-        <Box borderRadius="4px" overflow="hidden" bgColor="orange.400" h="170px" w="100%"/>
-        <Box borderRadius="4px" overflow="hidden" bgColor="orange.400" h="170px" w="100%"/>
+      <Grid
+        templateColumns={["1fr","1fr 1fr", "repeat(3, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]}
+        gap={['20px','45px']} alignItems="center"
+        justifyContent="center"
+        px={["30px", "0"]}
+      >
+        <City city="Paris" country="França"/>
+        <City city="Londres" country="Reino Unido"/>
+        <City city="Roma" country="Itália"/>
+        <City city="Praga" country="República Tcheca"/>
+        <City city="Amsterdã" country="Holanda"/>
       </Grid>
     </>
   )
