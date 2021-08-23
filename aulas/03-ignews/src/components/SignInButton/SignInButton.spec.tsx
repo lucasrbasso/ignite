7e78079c59ad793a1dbe39b67ac410fe44e161e0
lucasrbasso <lucas.rbasso@outlook.com>
2021-08-23 12:22:11 -0300

    new tests

--- aulas/03-ignews/src/components/SignInButton/SignInButton.spec.tsx
+++ aulas/03-ignews/src/components/SignInButton/SignInButton.spec.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { mocked } from 'ts-jest/utils';
 import React from 'react';
-import { useSession } from 'next-auth/client';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { SignInButton } from '.';
 
 jest.mock('next-auth/client');
@@ -9,11 +9,17 @@ jest.mock('next-auth/client');
 describe('SignInButton component', () => {
   it('should be able to SignInButton component renders correctly when user is not authenticated', () => {
     const useSessionMocked = mocked(useSession);
+    const signInMocked = mocked(signIn);
 
     useSessionMocked.mockReturnValueOnce([null, false]);
 
     render(<SignInButton />);
 
+    const signInButton = screen.getByText('Sign in with Github');
+
+    fireEvent.click(signInButton);
+
+    expect(signInMocked).toHaveBeenCalledWith('github');
     expect(screen.getByText('Sign in with Github')).toBeInTheDocument();
   });
 
@@ -34,5 +40,30 @@ describe('SignInButton component', () => {
     render(<SignInButton />);
 
     expect(screen.getByText('John Doe')).toBeInTheDocument();
+  });
+
+  it('should logout user on click', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+        },
+        expires: 'fake-expires',
+      },
+      false,
+    ]);
+
+    const signOutMocked = mocked(signOut);
+
+    render(<SignInButton />);
+
+    const signOutButton = screen.getByText('John Doe');
+
+    fireEvent.click(signOutButton);
+
+    expect(signOutMocked).toHaveBeenCalled();
   });
 });
