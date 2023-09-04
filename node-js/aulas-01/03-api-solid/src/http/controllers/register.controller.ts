a3a08c00b93bc22e785ee417eccfaf1886d380c4
lucasrbasso <lucas.rbasso@outlook.com>
2023-09-04 19:46:41 -0300

    feat: creating first routes

---
+++ node-js/aulas-01/03-api-solid/src/http/controllers/register.controller.ts
@@ -0,0 +1,27 @@
+import { prisma } from '@/lib/prisma'
+import { registerUseCase } from '@/use-cases/register.use-case'
+import { hash } from 'bcryptjs'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409)
+  }
+
+  return reply.status(201).send()
+}
---
+++ node-js/aulas-01/03-api-solid/src/use-cases/register.use-case.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseParams {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseParams) {
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('e-mail already exists.')
+  }
+
+  const password_hash = await hash(password, 6)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+}
