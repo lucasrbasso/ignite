7fafb2480a62eec23d282525c060d6411e1c41ff
lucasrbasso <lucas.rbasso@outlook.com>
2023-10-30 20:19:39 -0300

    feat: new forum domain

---
+++ node-js/aulas-01/04-clean-ddd/src/domain/entities/answer.ts
@@ -0,0 +1,21 @@
+import { randomUUID } from "node:crypto";
+
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
+}
+
+export class Answer {
+  public content: string
+  public id: string
+  public authorId: string
+  public questionId: string
+  
+  constructor(props: AnswerProps,id?: string) {
+    this.content = props.content
+    this.id = id ?? randomUUID();
+    this.authorId = props.authorId;
+    this.questionId = props.questionId;
+  }
+}
