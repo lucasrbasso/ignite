7a13a3509105a77be3b131e74203941dff4f9e95
lucasrbasso <lucas.rbasso@outlook.com>
2023-11-10 20:57:59 -0300

    feat: creating some use-cases

---
+++ node-js/aulas-01/04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,28 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Find question by slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to find a question by slug', async () => {
+    const newQuestion = makeQuestion({
+      slug: Slug.create('example-question'),
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.slug.value).toEqual('example-question')
+  })
+})
